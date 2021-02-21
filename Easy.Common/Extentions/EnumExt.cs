52a8b2e38e69aa0f61f9f46e332824e13f4adcf5
hy <hy@live.com>
2021-02-21 16:34:34 +0800

    11

--- Easy.Common/Extentions/EnumExt.cs
+++ Easy.Common/Extentions/EnumExt.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Reflection;
 using System.Linq;
+using System.Web.Mvc;
 
 namespace Easy.Common.Extentions
 {
@@ -37,18 +38,83 @@ namespace Easy.Common.Extentions
             return isOk;
         }
 
-        public static List<string> GetEnumList(this Type enumType, bool isNeedAddAll = true)
+        public static Dictionary<string, int?> GetEnumList(this Type enumType, bool isNeedAddAll = true)
         {
             if (!enumType.IsEnum)
             {
-                return new List<string>();
+                return new Dictionary<string, int?>();
             }
 
-            var list = enumType.GetEnumNames().ToList();
+            var dic = new Dictionary<string, int?>();
 
             if (isNeedAddAll)
             {
-                list.Insert(0, "全部");
+                dic.Add("全部", null);
+            }
+
+            foreach (var value in enumType.GetEnumValues())
+            {
+                string enumName = Enum.GetName(enumType, value);
+                dic.Add(enumName, (int)value);
+            }
+
+            return dic;
+        }
+
+        /// <summary>
+        /// 获取枚举列表
+        /// </summary>
+        /// <param name="mustKeyWord">必须含有的关键字（用来剔除，只保留部分选项）</param>
+        public static List<SelectListItem> GetEnumList(this Enum enumValue, Enum selectedValue = null, bool isNeedAddAll = true,
+            string mustKeyWord = "")
+        {
+            var list = new List<SelectListItem>();
+
+            Type type = enumValue.GetType();
+
+            foreach (int value in Enum.GetValues(type))
+            {
+                string name = Enum.GetName(type, value);
+
+                if (!string.IsNullOrWhiteSpace(mustKeyWord))
+                {
+                    //剔除不符合的
+                    if (name.Contains(mustKeyWord))
+                    {
+                        list.Add(new SelectListItem { Text = name, Value = value.ToString() });
+                    }
+                }
+                else
+                {
+                    list.Add(new SelectListItem { Text = name, Value = value.ToString() });
+                }
+            }
+
+            if (isNeedAddAll)
+            {
+                list.Insert(0, new SelectListItem { Text = "全部", Value = null });
+            }
+
+            if (selectedValue != null)
+            {
+                var find = list.Where(i => (Convert.ToInt32(selectedValue).ToString() == i.Value));
+
+                if (find.Any())
+                {
+                    find.First().Selected = true;
+                }
+            }
+            else
+            {
+                if (isNeedAddAll)
+                {
+                    var find = list.Where(i => null == i.Value);
+
+                    if (find.Any())
+                    {
+                        find.First().Selected = true;
+                    }
+                }
             }
 
             return list;
